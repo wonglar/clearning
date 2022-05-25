3e70adf4d32de85fb8f75652e2f4de68b7808378
lar <wonglar@163.com>
2022-05-25 16:32:08 +0800

    test

--- Test/Demo05.c
+++ Test/Demo05.c
@@ -1,14 +1,15 @@
-/*
-	内存操作函数
-*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-
+/*
+内存操作函数
+*/
 int main()
 {
+	// 定义一个字符串
 	char arr[] = "hello world";
 	int len = strlen(arr);
 	printf("len = %d\n", len);
+
 	return 0;
 }
