499bf996d4a93de174e726c5a902590bbcbbb5db
lar <wonglar@163.com>
2022-05-25 16:03:29 +0800

    test

---
+++ Test/Demo05.c
@@ -0,0 +1,14 @@
+/*
+	ÄÚ´æ²Ù×÷º¯Êý
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char arr[] = "hello world";
+	int len = strlen(arr);
+	printf("len = %d\n", len);
+	return 0;
+}
