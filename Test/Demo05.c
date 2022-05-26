1da95892e0d9e4270d0c5aeeda5262984e0ae2ff
wonglar <wonglar@163.com>
2022-05-26 20:32:57 +0800

    内存操作函数1

--- Test/Demo05.c
+++ Test/Demo05.c
@@ -4,15 +4,41 @@
 /*
 内存操作函数
 */
+
+
+// memcpy()    void * memcpy ( void * destination, const void * source, size_t num );
+//int main()
+//{
+//	int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int arr2[10] = {0};
+//
+//	memcpy(arr2, arr1, 5 * sizeof(int));
+//
+//	return 0;
+//}
+
+#include <assert.h>
+void* my_memcpy(void* dest, const void* src, size_t num)
+{
+	assert(dest && src);
+	void* ret = dest;
+	while (num--)
+	{
+		*(char*)dest = *(char*)src;
+		dest = (char*)dest + 1;
+		src = (char*)src + 1;
+	}
+
+	return ret;
+}
+
+// 模拟实现memcpy()函数
 int main()
 {
-	// 定义一个字符串
-	char arr[] = "hello world";
-	int len = strlen(arr);
-	printf("len = %d\n", len);
-	printf("arr[3] = %c", arr[3]);
-	
-	printf("%p\n", &arr[0]);
+	int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+	int arr2[10] = {0};
+
+	my_memcpy(arr2, arr1, 5 * sizeof(int));
 
 	return 0;
 }
