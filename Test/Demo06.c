b2380c2341a88c2d76500b74607cf02d3de01189
wonglar <wonglar@163.com>
2022-05-27 20:15:41 +0800

    内存操作函数

---
+++ Test/Demo06.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*
+	自定义类型
+*/
+
+
+// 匿名结构体类型
+//struct
+//{
+//	char c;
+//	int i;
+//	char ch;
+//	double d;
+//}s;  // 直接创建了一个结构体变量s
+
+
+// 匿名结构体类型
+//struct
+//{
+//	char c;
+//	int i;
+//	char ch;
+//	double d;
+//} *ps;  // 匿名结构体指针
+
+
+// 观察下面代码是否可行？
+// 不行！
+//struct N
+//{
+//	int d;
+//	struct N n;
+//};
+
+// 结构体的自引用
+//struct Node
+//{
+//	int data;
+//	struct Node* next; // 找到自己同类型的下一个元素
+//};
+
+
+
+
+
+/*
+	结构体内存对齐
+*/
+
+int main()
+{
+
+	return 0;
+}
